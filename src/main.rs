854b48dcfbc07992dbbe40c93257cbb2fa41b55a
Oskar Wiszowaty <ppwiszu@gmail.com>
2024-06-01 10:12:05 +0200

    .env file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 mod templates;
 
+use dotenv;
+
 use askama_axum::IntoResponse;
 use axum::{routing::get, Router};
 use templates::IndexTemplate;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(root));
+    let app = Router::new().route("/", get(root));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:42069").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:42069")
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap()
 }
 
 async fn root() -> impl IntoResponse {
-    IndexTemplate{ name: "Stranger".to_string() }.into_response()
+    let key = "NAME";
+    let name = match dotenv::var(key) {
+        Ok(val) => val,
+        Err(_) => "strager".to_string(),
+    };
+    IndexTemplate { name }.into_response()
 }
